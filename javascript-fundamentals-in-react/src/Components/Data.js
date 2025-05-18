2dc3f35213bb5ccf56a719a4b2bac4a07e582e0c
Vaibhav29 <vaibhavtonge29@gmail.com>
2025-05-18 23:33:57 +0530

    react fundaments

---
+++ javascript-fundamentals-in-react/src/Components/Data.js
@@ -0,0 +1,6 @@
+export const data = [
+    { id: 1, name: 'Alice', age: 25, hobbies: ['Reading', 'Coding'] },
+    { id: 2, name: 'Bob', age: 30, hobbies: ['Music', 'Gaming'] },
+    { id: 3, name: 'Charlie', age: 22, hobbies: ['Drawing', 'Cycling'] },
+    { id: 4, name: 'Vaibhav', age: 27, hobbies: ['Gym', 'Coding'] }
+];
